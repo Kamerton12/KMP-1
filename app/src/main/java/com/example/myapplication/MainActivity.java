c3713400248b9992d5cb058b3c111a08a3dfd1b4
Maxim Drozd <s312@tut.by>
2019-01-30 22:37:29 +0300

    - fabric

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -4,22 +4,27 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
-import com.crashlytics.android.Crashlytics;
-import io.fabric.sdk.android.Fabric;
+//import com.crashlytics.android.Crashlytics;
+
+import java.io.IOException;
+import java.net.URL;
+
+//import io.fabric.sdk.android.Fabric;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Fabric.with(this, new Crashlytics());
+//        Fabric.with(this, new Crashlytics());
         setContentView(R.layout.activity_main);
 
 
-        Fabric.with(this, new Crashlytics());
+//        Fabric.with(this, new Crashlytics());
 
 
         final TextView version = findViewById(R.id.version);
         version.setText(BuildConfig.VERSION_NAME);
+        throw new ArrayIndexOutOfBoundsException();
     }
 }
